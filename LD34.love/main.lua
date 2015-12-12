69ec01679cd4f45f4707d60743c9af7c40b60cd6
Noah Witherspoon <mahalis@gmail.com>
2015-12-11 19:09:54 -0800

    Initial

---
+++ LD34.love/main.lua
@@ -0,0 +1,65 @@
+require "vectors"
+
+local elapsedTime
+local playing
+local gameOver
+
+function love.load()
+	math.randomseed(os.time())
+
+	reset()
+end
+
+function love.draw()
+	love.graphics.setColor(255, 255, 255, 255)
+	local w, h = love.window.getDimensions()
+end
+
+function love.update(dt)
+	elapsedTime = elapsedTime + dt
+
+end
+
+function reset()
+	playing = false
+	gameOver = false
+	elapsedTime = 0
+end
+
+function start()
+	playing = true
+end
+
+function endGame()
+	playing = false
+	gameOver = true
+end
+
+function love.mousepressed(x, y, button)
+	if not playing then
+		if gameOver then
+			reset()
+		else
+			start()
+		end
+	else
+		
+	end
+end
+
+function love.mousereleased(x, y, button)
+	if playing then
+		
+	end
+end
+
+function mixColorTables(a, b, f)
+	return {a[1] + f * (b[1] - a[1]), a[2] + f * (b[2] - a[2]), a[3] + f * (b[3] - a[3])}
+end
+
+-- sine-curve interpolation
+function slerp(a, b, f)
+	f = math.max(math.min(f, 1), 0)
+
+	return a + (b - a) * (1 - math.cos(f * math.pi)) / 2
+end
